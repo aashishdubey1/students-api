1c9c5c7bf8145833a3ec92f91c7f2fde433480ff
Aashish <aashishdubey.dev@gmail.com>
2024-11-18 14:13:41 +0530

    added configuration

---
+++ internal/config.go
@@ -0,0 +1,51 @@
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env          string `yaml:"env" env:"ENV" env-required:"true"`
+	StrogagePath string `yaml:"storage_path" env-required:"true"`
+	HttpServer   `yaml:"http_server"`
+}
+
+type HttpServer struct {
+	Addr string
+}
+
+func MustLoad() *Config {
+	var configPath string
+
+	configPath = os.Getenv("CONFIG_PATH")
+
+	if configPath == "" {
+		flags := flag.String("config", "", "path to the configuration file")
+		flag.Parse()
+
+		configPath = *flags
+
+		if configPath == "" {
+			log.Fatal("config path is not set")
+		}
+	}
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist: %s", configPath)
+	}
+
+	var cfg Config
+
+	err := cleanenv.ReadConfig(configPath, &cfg)
+
+	if err != nil {
+		log.Fatalf("can not read config file: %s", err.Error())
+	}
+
+	return &cfg
+
+}
